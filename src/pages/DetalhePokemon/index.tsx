92c1bee93d3bee5fc1f3b7ce662469a00003803c
anderson-alves427 <oliveiraalves427@gmail.com>
2022-12-30 02:27:28 -0300

    Tratando condiçao onde n tem dados de pokemon em detalhes

--- src/pages/DetalhePokemon/index.tsx
+++ src/pages/DetalhePokemon/index.tsx
@@ -13,45 +13,49 @@ export const DetalhePokemon = () => {
       <div className="back" onClick={() => navigate("/")}>
         <IoArrowBackOutline size={32}/>
       </div>
-      <h2 className="nome-pokemon">{pokemonSelecionado.forms[0].name}</h2>
-      <img className="imagem-pokemon-detalhe" src={pokemonSelecionado.sprites.front_default} alt={`imagem pokemon${pokemonSelecionado.forms[0].name}`} />
-      
-      <div className="card-detalhe-pokemon">
-        <div className="detalhe-primario">
-          <div className="habilidades">
-            <p className="p-detalhe">Habilidades</p>
-            <ul>
-              {pokemonSelecionado.abilities.map((habilidade, index) => (
-                <li className="descricao-atributos" key={index}>{habilidade.ability.name}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="tipo">
-            <p className="p-detalhe">Tipo</p>
-            <ul>
-            {pokemonSelecionado.types.map((type, index) => (
-                <li className="descricao-atributos" key={index}>{type.type.name}</li>
-              ))}
-            </ul>
-          </div>
-
-        </div>
-        <div className="atributos">
-          {pokemonSelecionado.stats.map((item, index) => (
-            <div className="span-atributos" key={index}>
-              <p className="descricao-atributos">{item.stat.name}</p>
-              <div className="container-atributo">
-                <div className="atributo-cor" 
-                  style={{
-                    width: `${(item.base_stat/120)*100}%`,
-                    backgroundColor: `${item.stat.name === "hp" ? "green" : item.stat.name === "attack" ? "red" : item.stat.name === "defense" ? "blue" : item.stat.name === "special-attack" ? "yellow": item.stat.name === "special-defense" ? "brown": "gray"}`
-                  }}
-                ></div>
+      {Object.keys(pokemonSelecionado).length === 0 ? (
+        <>
+          <h2 className="nome-pokemon">{pokemonSelecionado.forms[0].name}</h2>
+          <img className="imagem-pokemon-detalhe" src={pokemonSelecionado.sprites.front_default} alt={`imagem pokemon${pokemonSelecionado.forms[0].name}`} />
+          
+          <div className="card-detalhe-pokemon">
+            <div className="detalhe-primario">
+              <div className="habilidades">
+                <p className="p-detalhe">Habilidades</p>
+                <ul>
+                  {pokemonSelecionado.abilities.map((habilidade, index) => (
+                    <li className="descricao-atributos" key={index}>{habilidade.ability.name}</li>
+                  ))}
+                </ul>
               </div>
+              <div className="tipo">
+                <p className="p-detalhe">Tipo</p>
+                <ul>
+                {pokemonSelecionado.types.map((type, index) => (
+                    <li className="descricao-atributos" key={index}>{type.type.name}</li>
+                  ))}
+                </ul>
+              </div>
+
             </div>
-          ))}
-        </div>
-      </div>
+            <div className="atributos">
+              {pokemonSelecionado.stats.map((item, index) => (
+                <div className="span-atributos" key={index}>
+                  <p className="descricao-atributos">{item.stat.name}</p>
+                  <div className="container-atributo">
+                    <div className="atributo-cor" 
+                      style={{
+                        width: `${(item.base_stat/120)*100}%`,
+                        backgroundColor: `${item.stat.name === "hp" ? "green" : item.stat.name === "attack" ? "red" : item.stat.name === "defense" ? "blue" : item.stat.name === "special-attack" ? "yellow": item.stat.name === "special-defense" ? "brown": "gray"}`
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : <div>Nenhum registro encontrado, por favor, volte na tela anterior e selecione um novo pokemon.</div>}
     </section>
   );
 }
